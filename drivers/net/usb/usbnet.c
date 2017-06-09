d5c0e9d2b1ef05ebbff65b275e73fab669a46001
Tom Briden <tom@decompile.me.uk>
2017-06-09 10:06:40 +0100

    Add CONFIG_ANDROID around 'usbnet: [TEMP HACK] force the interface name to ethX'

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -1660,7 +1660,11 @@ usbnet_probe (struct usb_interface *udev, const struct usb_device_id *prod)
 	dev->interrupt_count = 0;
 
 	dev->net = net;
+#ifdef CONFIG_ANDROID
 	strcpy (net->name, "eth%d");
+#else
+	strcpy (net->name, "usb%d");
+#endif
 	memcpy (net->dev_addr, node_id, sizeof node_id);
 
 	/* rx and tx sides can use different message sizes;
